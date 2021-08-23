b43886a9b182d6bf669718c366432c6a0268ada9
Kyoishi <lamusiquedeki@gmail.com>
2021-08-23 08:21:20 -0700

    Wrote Time

--- problem2.py
+++ problem2.py
@@ -1,4 +1,18 @@
 
+import time
+
+# reference: Module 8 asynchronic lecture 
+class Timer:   
+
+    def __enter__(self):
+        self.start = time.time()
+        return self
+
+    def __exit__(self, *args):
+        self.end = time.time()
+        self.secs = self.end - self.start
+
+
 # Use the following list of temperature on each of your approaches:
 temperatures = [-32.0, 0.0, 1.0, 10.0, 32.0, 50.3, 88.8, 101.0]
 
@@ -8,16 +22,23 @@ temperatures = [-32.0, 0.0, 1.0, 10.0, 32.0, 50.3, 88.8, 101.0]
 def converter(x):
     return x * 9/5 + 32
 
-converted = list(map(converter, temperatures))
-print(converted)
+with Timer() as t1:
+    converted = list(map(converter, temperatures))
+    print(converted)
+
 
 # Write another statement using list comprehension
 # to make the same conversion.
 
-converted_temps = [(x * 9/5 + 32) for x in temperatures]
-print(converted_temps)
+with Timer() as t2:
+    converted_temps = [(x * 9/5 + 32) for x in temperatures]
+    print(converted_temps)
 
 # What is the time complexity of each approach? 
-# Answer: both of the statements are  
+# Answer: both of the statements are O(n)
 
-# Devise and conduct a benchmarking experiment to determine if one is faster than the other in practice.
+# Devise and conduct a benchmarking experiment
+# to determine if one is faster than the other in practice.
+
+print("Time for 1st statement is {}".format(t1.secs))
+print("Time for 2nd statement is {}".format(t2.secs))
